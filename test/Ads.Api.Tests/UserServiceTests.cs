2809d4152cf44d5013857d862049eb234061460f
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2019-05-05 22:34:49 +0300

    UserService tests

--- test/Ads.Api.Tests/UserServiceTests.cs
+++ test/Ads.Api.Tests/UserServiceTests.cs
@@ -1,13 +1,31 @@
+using System;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Ads.Api.Tests
 {
     public class UserServiceTest
     {
-        [Fact]
-        public void Test1()
+        private IUserService GetInMemoryUserService()
         {
+            DbContextOptions<AdsDbContext> options;
+            var builder = new DbContextOptionsBuilder<AdsDbContext>();
+            options = builder.UseInMemoryDatabase(new Guid().ToString()).Options;
 
+            AdsDbContext clubSystemDbContext = new AdsDbContext(options);
+            clubSystemDbContext.Database.EnsureDeleted();
+            clubSystemDbContext.Database.EnsureCreated();
+            return new UserService(clubSystemDbContext);
+        }
+        
+        [Fact]
+        public void ShouldThrowLoginError()
+        {
+            // arrange
+
+            // act
+
+            // assert
         }
     }
 }
