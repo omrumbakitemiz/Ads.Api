a5997eab8fbc868269af97133d5cbecae0fa2345
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2019-05-26 21:51:33 +0300

    remove authorize

--- Controllers/CampaignController.cs
+++ Controllers/CampaignController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Ads.Api.Controllers
 {
     [Route("api/[controller]")]
-    [Authorize]
+    [ApiController]
     public class CampaignController : ControllerBase
     {
         private readonly ICampaignService _campaignService;
